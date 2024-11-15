bd673f2b40fbc5c74da340d2e3ec65081025eb87
Brad Traversy <traversymedia@gmail.com>
2024-11-15 10:17:45 -0500

    Product card component

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,3 +1,5 @@
+import ProductCard from './product-card';
+
 const ProductList = ({ data, title }: { data: any; title?: string }) => {
   return (
     <div className='my-10'>
@@ -6,7 +8,7 @@ const ProductList = ({ data, title }: { data: any; title?: string }) => {
         <div>
           <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
             {data.map((product: any) => (
-              <div>{product.name}</div>
+              <ProductCard product={product} key={product.slug} />
             ))}
           </div>
         </div>
