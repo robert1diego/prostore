8f55ed3ae02473c0ec268b01072c4cba02d4b3d4
Brad Traversy <traversymedia@gmail.com>
2024-11-15 08:10:20 -0500

    List sample products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,13 @@
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/db/sample-data';
+
 const Homepage = () => {
-  return <>Prostore</>;
+  return (
+    <div className='space-y-8'>
+      <h2 className='h2-bold'>Latest Products</h2>
+      <ProductList title='Newest Arrivals' data={sampleData.products} />
+    </div>
+  );
 };
 
 export default Homepage;
---
+++ components/shared/product/product-list.tsx
@@ -0,0 +1,22 @@
+const ProductList = ({ data, title }: { data: any; title?: string }) => {
+  return (
+    <div className='my-10'>
+      <h2 className='h2-bold mb-4'>{title}</h2>
+      {data.length > 0 ? (
+        <div>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+            {data.map((product: any) => (
+              <div>{product.name}</div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div>
+          <p>No products found</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
