a358ac28bbf5797c3b460bea8d7a6962fcdcbbfb
Brad Traversy <traversymedia@gmail.com>
2024-12-02 16:51:24 -0500

    getOrderSummary action

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -286,3 +286,43 @@ export async function getMyOrders({
     totalPages: Math.ceil(dataCount / limit),
   };
 }
+
+type salesDataType = {
+  month: string;
+  totalSales: number;
+};
+
+// Get sales data and order summary
+export async function getOrderSummary() {
+  // Get counts for each resource
+  const ordersCount = await prisma.order.count();
+  const productsCount = await prisma.product.count();
+  const usersCount = await prisma.user.count();
+
+  // Calculate total sales
+  const totalSales = await prisma.order.aggregate({
+    _sum: { totalPrice: true },
+  });
+
+  // Get monthly sales
+  const salesData =
+    await prisma.$queryRaw`SELECT to_char("createdAt", 'MM/YY') as "month", sum("totalPrice") as "totalSales" FROM "Order" GROUP BY to_char("createdAt", 'MM/YY')`;
+
+  // Get latest sales
+  const latestOrders = await prisma.order.findMany({
+    orderBy: { createdAt: 'desc' },
+    include: {
+      user: { select: { name: true } },
+    },
+    take: 6,
+  });
+
+  return {
+    ordersCount,
+    productsCount,
+    usersCount,
+    totalSales,
+    latestOrders,
+    salesData: salesData as salesDataType,
+  };
+}
