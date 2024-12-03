91f312ea6f80358c71397106a36cefa13594fa3b
Brad Traversy <traversymedia@gmail.com>
2024-12-03 14:51:20 -0500

    Fix:Escape apostrophe

--- components/shared/delete-dialog.tsx
+++ components/shared/delete-dialog.tsx
@@ -54,7 +54,7 @@ const DeleteDialog = ({
         <AlertDialogHeader>
           <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
           <AlertDialogDescription>
-            This action can't be undone
+            This action cannot be undone
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
