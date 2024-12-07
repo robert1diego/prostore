7a96c3db8946e7fecde386ee5136f0822e810cbe
Brad Traversy <traversymedia@gmail.com>
2024-12-07 18:19:49 -0500

    Search page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,7 @@ import {
   getFeaturedProducts,
 } from '@/lib/actions/product.actions';
 import ProductCarousel from '@/components/shared/product/product-carousel';
+import ViewAllProductsButton from '@/components/view-all-products-button';
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
@@ -15,6 +16,7 @@ const Homepage = async () => {
         <ProductCarousel data={featuredProducts} />
       )}
       <ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
+      <ViewAllProductsButton />
     </>
   );
 };
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -38,11 +38,17 @@ export async function getAllProducts({
   limit = PAGE_SIZE,
   page,
   category,
+  price,
+  rating,
+  sort,
 }: {
   query: string;
   limit?: number;
   page: number;
   category?: string;
+  price?: string;
+  rating?: string;
+  sort?: string;
 }) {
   const data = await prisma.product.findMany({
     orderBy: { createdAt: 'desc' },
