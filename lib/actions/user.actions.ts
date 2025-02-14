9ced2446cd15ebcdba464dcee8e2265b2915b50d
Brad Traversy <traversymedia@gmail.com>
2025-02-14 10:23:27 -0500

    Check for cart before deleting

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -45,9 +45,15 @@ export async function signInWithCredentials(
 export async function signOutUser() {
   // get current users cart and delete it so it does not persist to next user
   const currentCart = await getMyCart();
-  await prisma.cart.delete({ where: { id: currentCart?.id } });
+
+  if (currentCart?.id) {
+    await prisma.cart.delete({ where: { id: currentCart.id } });
+  } else {
+    console.warn('No cart found for deletion.');
+  }
   await signOut();
 }
+
 // Sign up user
 export async function signUpUser(prevState: unknown, formData: FormData) {
   try {
