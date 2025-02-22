14ab015616d6377a223d6da8361eb4ab9b0afc75
Will Adams <bushblade@gmail.com>
2025-02-22 17:21:06 +0000

    correction: use of require in ts/es module

--- email/index.tsx
+++ email/index.tsx
@@ -1,7 +1,9 @@
 import { Resend } from 'resend';
 import { SENDER_EMAIL, APP_NAME } from '@/lib/constants';
 import { Order } from '@/types';
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
+
 import PurchaseReceiptEmail from './purchase-receipt';
 
 const resend = new Resend(process.env.RESEND_API_KEY as string);
