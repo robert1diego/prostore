38be29156dda1fb29b00de4729b65e6a71b8346d
Will Adams <bushblade@gmail.com>
2025-03-15 11:47:33 +0000

    fix: isRedirectError import on next@15.2.2

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -8,7 +8,7 @@ import {
   updateUserSchema,
 } from '../validators';
 import { auth, signIn, signOut } from '@/auth';
-import { isRedirectError } from 'next/dist/client/components/redirect';
+import { isRedirectError } from 'next/dist/client/components/redirect-error';
 import { hash } from '../encrypt';
 import { prisma } from '@/db/prisma';
 import { formatError } from '../utils';
